91d2ddd114d70b8ff0d47e625274004f5d280e16
Brigham Johns <brighamjohns@gmail.com>
2025-11-14 13:48:09 -0800

    Remove redundant info function from box_shooter.py and restore it in descriptions.py

--- box_shooter.py
+++ box_shooter.py
@@ -263,15 +263,5 @@ def main():
     pygame.quit()
     sys.exit()
 
-def info(option=3, challenge=1):
-    if option == 1 or option == 3:
-        print("A box shooter game where you play as")
-        print("the white box and shoot down the red ")
-        print("boxes to prevent them from reaching ")
-        print("the bottom of the pygame window.")
-    if option == 2 or option == 3:
-        
-
-
 if __name__ == "__main__":
     main()
