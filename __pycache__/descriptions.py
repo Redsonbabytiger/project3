579faa2e6a0b5a551a2b0269a02cf58dca680f55
Redsonbabytiger <84416518+Redsonbabytiger@users.noreply.github.com>
2025-12-12 17:04:15 -0800

    Delete __pycache__ directory

--- __pycache__/descriptions.py
+++
@@ -1,8 +0,0 @@
-def info(option=3, challenge=1):
-    if option == 1 or option == 3:
-        print("A box shooter game where you play as")
-        print("the white box and shoot down the red ")
-        print("boxes to prevent them from reaching ")
-        print("the bottom of the pygame window.")
-    if option == 2 or option == 3:
-        
